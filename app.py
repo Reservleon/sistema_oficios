3757dd8fade61ab3c8a30df5c59f76e9b7130c06
Reservleon <cff238845@gmail.com>
2026-09-14 15:57:21 -0300

    Update app.py
    remoção de emoji

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ with st.form("form_oficio", clear_on_submit=False):
 st.divider()
 
 # Tabela de Consulta em Tempo Real
-st.subheader("📋 Ofícios Registrados")
+st.subheader("Ofícios Registrados")
 
 # Busca todos os dados no banco de dados
 cursor = conn.cursor()
